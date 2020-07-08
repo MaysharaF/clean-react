a5f0de76e54860eded11b5bec3aa28bd83a57d08
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-07-07 21:06:43 -0300

    feat: add authetication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,10 @@
+import { AccountModel } from "../models/account-models";
+
+type AuthenticationParams = {
+  email: string;
+  password: string;
+};
+
+export interface Authetication {
+  auth(param: AuthenticationParams): Promise<AccountModel>;
+}
