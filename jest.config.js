a9c14758a604e4722aa08092064d1e8df8ff5801
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-07-07 21:55:00 -0300

    fix: configure jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}"],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/src/main/test/cypress',
+  ],
+  testEnvironment: 'jsdom',
   transform: {
-    ".+\\.(ts|tsx)$": "ts-jest",
+    '.+\\.(ts|tsx)$': 'ts-jest',
   },
-};
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy',
+  },
+}
