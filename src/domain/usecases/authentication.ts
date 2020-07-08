a651c73cb4565e18954d941b2184213fdcbdf4da
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-07-07 21:23:29 -0300

    fix: ignore cypress folder for tsconfig

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,12 +1,12 @@
-import { AccountModel } from "../models/account-models";
+import { AccountModel } from '../models/account-models'
 
 type AuthenticationParams = {
-  email: string;
-  password: string;
-};
-
-export interface Authetication {
-  auth(param: AuthenticationParams): Promise<AccountModel>;
+  email: string
+  password: string
 }
 
-const teste;
+export interface Authetication {
+  auth(param: AuthenticationParams): Promise<AccountModel>
+}
+
+const test
