73e52c090cfa964ac4c5d595e433f6dcd700f890
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-07-18 23:50:29 -0300

    test: ensure Login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -5,9 +5,19 @@ import Login from './login'
 describe('Login Component', () => {
   test('should start with inital state ', () => {
     const { getByTestId } = render(<Login />)
+    // erro-wrap
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    // submit button
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+    // email status
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('❌')
+    // password status
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('❌')
   })
 })
