cfd599cd1f396eeb85eefc06c8f0a7eb8a6b7eaf
Wirlley Delfino <wirlley_d@hotmail.com>
2020-08-05 19:44:07 -0300

    feat: ensure Login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -21,10 +21,10 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '❌'
+    return error ? '❌' : '✔'
   }
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo certo!'
   }
 
   return (
