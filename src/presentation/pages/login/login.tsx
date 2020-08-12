558ee78d1880eadb3509f94712651861b2c391a5
Wirlley Delfino <wirlley_d@hotmail.com>
2020-08-12 20:29:33 -0300

    feat: ensure Login add accessToken to localstorage on success

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -42,10 +42,11 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
         return
       }
       setState({ ...state, isLoading: true })
-      await authentication.auth({
+      const account = await authentication.auth({
         email: state.email,
         password: state.password
       })
+      localStorage.setItem('accessToken', account.accessToken)
     } catch (error) {
       setState({
         ...state,
